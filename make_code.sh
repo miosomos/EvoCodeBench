27efdeaaffe0ff4e33da3faf105d33940c47c97a
Omar Alejandro Guzmán Munguía <omaralexguzmanm21@gmail.com>
2025-06-24 23:42:25 -0600

    feat: add scripts for generating prompts and executing commands

---
+++ make_code.sh
@@ -0,0 +1,2 @@
+
+python gpt_inference.py --prompt_file prompt.jsonl --output_dir model_completion/kg/claude-4-sonnet --model claude-4-sonnet --moda greedy --api_key_file api_key.txt 
---
+++ make_prompt.sh
@@ -0,0 +1,4 @@
+Root=/home/omarguzman/EvoCodeBench
+
+
+python3 make_prompt.py --setting kg --output_file prompt.jsonl --tools cypher semantic --context_window 16384 --max_tokens 500 
